1810561f2f01bd9ec9d52f2b3c245e26ea6cfea2
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2018-09-13 19:13:33 +0900

    Accept name=class in env

--- datas_utils/env/env.py
+++ datas_utils/env/env.py
@@ -1,18 +1,28 @@
 """
 Usage:
     import env
-    e = env.load("ENVVAR_NAME1", "ENVVAR_NAME2")
+    e = env.load("ENVVAR_NAME1", "ENVVAR_NAME2", ENVVAR_BOOL=bool)
     print(e.ENVVAR_NAME1)
 """
 import os
+import distutils.util
 
 class Env(dict):
-    def __init__(self, varnames):
-        for varname in varnames:
+    def __init__(self, *args, **kwargs):
+        for varname in args:
             self[varname] = os.environ.get(varname)
+        for varname, varclass in kwargs.items():
+            varval = os.environ.get(varname)
+            if varval is None:
+                raise ValueError("Env. var. '{0}' of '{1}' type is undefined"\
+                                 .format(varname, varclass))
+            if varclass is bool:
+                varval = distutils.util.strtobool(varval)
+            self[varname] = varclass(varval)
 
     def __getattr__(self, key):
         return self.get(key)
 
-def load(varnames):
-    return Env(varnames)
+
+def load(*args, **kwargs):
+    return Env(*args, **kwargs)
--- tests/env_test.py
+++ tests/env_test.py
@@ -1,13 +1,66 @@
 import os
 from unittest import TestCase
-from nose.tools import eq_
+from nose.tools import eq_, ok_, raises
 from datas_utils import env
 
 class EnvTestCase(TestCase):
 
-    def test_env(self):
+    @classmethod
+    def setup_class(cls):
         os.environ["A"] = "X"
-        e = env.load(["A", "B"])
+        os.environ["D"] = "30"
+        os.environ["F"] = "3.01"
+        os.environ["G"] = "True"
+        os.environ["H"] = "False"
+        os.environ["I"] = "true"
+        os.environ["J"] = "false"
+        os.environ["K"] = "o"
+
+    def test_defined(self):
+        e = env.load("A", "B")
         eq_(e.A, "X")
+
+    def test_undefined(self):
+        e = env.load("A", "B")
         eq_(e.B, None)
         eq_(e.C, None)
+
+    def test_int(self):
+        e = env.load(D=int)
+        ok_(isinstance(e.D, int))
+        eq_(e.D, 30)
+
+    @raises(ValueError)
+    def test_undefined_int(self):
+        env.load(B=int)
+
+    @raises(ValueError)
+    def test_invalid_int(self):
+        env.load(A=int)
+
+    def test_float(self):
+        e = env.load(F=float)
+        ok_(isinstance(e.F, float))
+        eq_(e.F, 3.01)
+
+    def test_capital_bool(self):
+        e = env.load(G=bool, H=bool)
+        ok_(isinstance(e.G, bool))
+        ok_(isinstance(e.H, bool))
+        ok_(e.G, "must be True")
+        ok_(not e.H, "must be False")
+
+    def test_lower_bool(self):
+        e = env.load(I=bool, J=bool)
+        ok_(isinstance(e.I, bool))
+        ok_(isinstance(e.J, bool))
+        ok_(e.I, "must be True")
+        ok_(not e.J, "must be False")
+
+    @raises(ValueError)
+    def test_invalid_bool(self):
+        env.load(K=bool)
+
+    @raises(ValueError)
+    def test_undefined_bool(self):
+        env.load(L=bool)
