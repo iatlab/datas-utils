9e0061f83436b15574740270ff737385077a5eb9
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2018-09-13 22:59:07 +0900

    Add modules in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup
 
 setup(
     name = "datas_utils",
-    packages = ["datas_utils", "datas_utils.env"],
+    packages = ["datas_utils", 
+                "datas_utils.env",
+                "datas_utils.log",
+                "datas_utils.aws",
+               ],
     version = "0.0.1",
     description = "Tools for Datas Project",
     author = "Makoto P. Kato",
